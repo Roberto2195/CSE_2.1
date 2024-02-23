8563b195186a842b7539d3c4a1b156fd03bc4423
R. Cruz <roberto.cruz@vtsd.com>
2024-02-23 13:11:16 -0500

    Setup CSE 2.1 Repo

--- 2.1.1/u311.py
+++ 2.1.1/u311.py
@@ -1,12 +1,13 @@
-eye =  "blue"
+eye = "blue"
 eye = "green"
 eye = "red"
-
 def driveFoward():
     print("Drive forward")
-
+def turnRight():
+    print("Turn right")
+def turnLeft():
+    print("Turn Left")
 def driveByColor():
-
     while(True):
         driveFoward()
         if(eye == "green"):
@@ -14,5 +15,11 @@ def driveByColor():
         if(eye == "blue"):
             turnLeft()
         if (eye == "red"):
-            # Stop here
-
+            #Stop here
+'''
+Drive based on what color the robot sees
+'''
+def text_prompt():
+    name = input("What is your name?")
+    print("Hello" + name)
+text_prompt()
