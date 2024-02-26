5a52ac65873a5c509b1009210a740f7fbaca9be8
R. Cruz <roberto.cruz@vtsd.com>
2024-02-26 12:28:49 -0500

    Setup CSE 2.1 Repo

---
+++ 2.1.1/loopy.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter a number below: "))
+num2 = input("Enter a second number: ")
+
+if num1 == 49:
+    print("Hello")
---
+++ Challenge1.py
@@ -0,0 +1,12 @@
+'''
+Write a program that stores 2 numbers (from user input) as variables (num1 & 2)
+
+Check if num1 is 49, if yes print Hello
+'''
+num1 = int(input("Enter a number below: "))
+num2 = input("Enter a second number: ")
+
+if num1 == 49:
+    print("Hello")
+elif num2 == 42:
+    print("Hello")
