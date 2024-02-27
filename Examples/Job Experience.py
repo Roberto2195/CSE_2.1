19492730d81f2a4ef40c42b7fcec3942f920237f
R. Cruz <roberto.cruz@vtsd.com>
2024-02-27 11:45:26 -0500

    Setup CSE 2.1 Repo

---
+++ Examples/Job Experience.py
@@ -0,0 +1,11 @@
+degree = True
+experience = 3
+
+if degree == True:
+    if experience > 5:
+        print("Congratulations! We would like to hire as a manager")
+else:
+    print("Congratulations! We would like to hire you for an introductory position.")
+
+else:
+    print("Sorry, but yolu are not qualified for this job.")
---
+++ Examples/conditionChallenges.py
@@ -0,0 +1,14 @@
+meat = True
+cheese = True
+lettuce = 3
+
+
+
+if meat == False:
+    print("The meat is not in the burger. Please add meat.")
+
+if cheese == False:
+    print("Cheese is not on the burger. Please select if you would like to add cheese.")
+
+if lettuce != 1:
+    print("Don't forget to add at least 1 piece of lettuce.")
