59624a52c1a49fce4ff063d5f4ae76e122c5e49c
R. Cruz <roberto.cruz@vtsd.com>
2024-02-29 13:08:41 -0500

    Setup CSE 2.1 Repo

---
+++ Examples/Variables and Conditionals.py
@@ -0,0 +1,23 @@
+song = 2 + 1
+song = 3.0
+song = "geese"
+print(song)
+
+print(type("1"))
+print(type(-10))
+print(type(False))
+print(type("False"))
+print(type(3.14))
+
+x = input("What is your name?")
+y = input("What is your age?")
+
+print("Hello ", x)
+print("You are ", y, "years old")
+
+first_age = input("What is the age of person 1? ")
+second_age = input("What is the age of person 2?  ")
+
+difference = int(first_age) - int(second_age)
+
+print("Person 1 is", difference, "years older than person 2.")
