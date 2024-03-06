a27be17d0dde62c9ecdc68473d6cdc55c514784e
R. Cruz <roberto.cruz@vtsd.com>
2024-03-06 12:37:08 -0500

    Setup CSE 2.1 Repo

--- Examples/conditionChallenges.py
+++ Examples/conditionChallenges.py
@@ -1,14 +1,35 @@
-meat = True
-cheese = True
-lettuce = 3
-
-# Code begins here
-
-if meat == False:
-    print("The meat is not in the burger. Please add meat.")
-
-if cheese == False:
-    print("Cheese is not on the burger. Please select if you would like to add cheese.")
-
-if lettuce != 1:
-    print("Don't forget to add at least 1 piece of lettuce.")
+sandwhich_type = ""
+sandwhich_price = ""
+fry_type = ""
+beverage_type = ""
+subtotal = 0.00
+print("What type of sandwhich would you like?")
+print("chicken $5.25, beef $6.25, tofu $5.75")
+sandwhich_type = input("Sandwhich Choice: ")
+if sandwhich_type == "chicken":
+    print("You chose chicken, which will be $5.25")
+    subtotal += 5.25
+elif sandwhich_type == "Tofu":
+    print("You chose tofu, which will be $5.75")
+    subtotal += 5.75
+else:
+    print("You chose beef, which will be $6.25")
+    subtotal += 6.25
+print("Would you like a drink?")
+if input("Yes")
+    print("Small $1.00, Medium $1.75, Large $2.25")
+if beverage_type == "Small":
+    print("You chose small, which will be $1.00")
+    subtotal += 1.00
+print("Would you like to add fries?")
+if input("Yes"):
+    print("Small $1.00, medium $1.50, Large $2.00")
+if fry_type == "Small":
+    print("You chose small, which will be $1.00")
+    subtotal += 1.00
+elif fry_type == "Medium":
+    print("You chose medium, which will be $1.50")
+    subtotal += 1.50
+else:
+    print("You chose large, which will be $2.00")
+    subtotal += 2.00
