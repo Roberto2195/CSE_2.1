54d03376414e1afc55180db56739310ed4d3e2fc
R. Cruz <roberto.cruz@vtsd.com>
2024-03-15 11:45:04 -0400

    Setup CSE 2.1 Repo

---
+++ u322/u322.py
@@ -0,0 +1,13 @@
+import random
+
+def random_number():
+    n1 = random.randint(a=30,b=50)
+    print(n1)
+
+for x in range(12):
+    random_number()
+
+x = 0
+while x != 100:
+    n1 = random.randint(a=30,b=50)
+    x += 1
