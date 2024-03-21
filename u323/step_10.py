b7b1b62f0030d4ccad1b192645c096da25f732b8
R. Cruz <roberto.cruz@vtsd.com>
2024-03-21 11:45:30 -0400

    Setup CSE 2.1 Repo

---
+++ u323/step_10.py
@@ -0,0 +1,12 @@
+for number in range(1, 19):
+    print (number)
+
+for number in range(2, 11, 2):
+    print (number)
+
+for number in range(20, 1, -1):
+    print (number)
+
+for number in range(9, 0, -2):
+    print (number)
+
---
+++ u323/step_12_while_loops.py
@@ -0,0 +1,19 @@
+x = 0
+while (x < 11):
+  print(x)
+  x = x + 1
+
+x = 1
+while (x < 6):
+  print(x)
+  x = x + 1
+
+x = 10
+while (x > 0):
+  print(x)
+  x = x - 1
+
+x = 2
+while (x < 11):
+  print(x)
+  x = x + 1
