17ae0599c38b4c6e010ef19bd909e140870ef5ac
R. Cruz <roberto.cruz@vtsd.com>
2024-03-22 13:54:54 -0400

    Setup CSE 2.1 Repo

---
+++ u323/math.py
@@ -0,0 +1,11 @@
+days = 2000000000
+
+years = int(2000000000 / 365.25)
+
+days = 2000000000 % 365.25
+
+print(type(days))
+
+print("Minutes:" + str(minutes) + "Seconds:" + str(second))
+
+print("Years elapsed: ", 2000000000 / 365.25)
