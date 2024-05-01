7352065602d0e8826e461eb02df426b7fb75db25
R. Cruz <roberto.cruz@vtsd.com>
2024-05-01 13:10:44 -0400

    Setup CSE 2.1 Repo

--- u331/Essays.py
+++
@@ -1,2 +0,0 @@
-# Insert the 3 poems w/questions about lit. devices, relations to other works, etc
-print("Today, you will work on a practice AP exam for Literature & Composition. You will have a total of ")
---
+++ u331/main.py
@@ -0,0 +1,9 @@
+print("Question 1. Analyze each poem for the speaker's attitude toward his or her audience.")
+print("What impressions are made by the speaker? What assumptions can be made about the intended audience?")
+print("Use diction, detail and syntax to make your case.")
+
+
+import duchess
+
+enter_essay = input("Enter your written essay below: ")
+
