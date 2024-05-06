98eb9f7bd1936a9b5f6ad6ec845630ac15e603f2
R. Cruz <roberto.cruz@vtsd.com>
2024-05-06 11:45:41 -0400

    Setup CSE 2.1 Repo

--- u331/teacher_instructor_grading.py
+++ u331/teacher_instructor_grading.py
@@ -2,8 +2,23 @@
 print("Please grade your students test submissions below.")
 
 print("Below is the criteria for a written thesis statement. Please grade accordingly.")
-def points(input_essay_score):
-    if input_essay_score == 0:
-        print("0 points for a written thesis statement.")
-    elif input_essay_score == 1:
-        print("1 point for a written thesis statement.")
+total_grade = 0.0
+essay_score = input("Essay grade: ")
+essay_1 = True
+essay_2 = True
+Thesis_statement = True
+ev_and_comm = True
+soph_grade = True
+
+Thesis_grade = input("Please enter the grade for the written thesis: ")
+if Thesis_statement == "0":
+    print("Grade: 0")
+    total_grade += 0
+elif Thesis_statement == "1"
+    print("Grade: 1")
+    total_grade += 1
+
+ev_and_comm = input("Please enter the grade for evidence & commentary: ")
+if ev_and_comm == "0":
+    print("Grade: 0")
+    total_grade += 0
