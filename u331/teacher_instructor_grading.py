ce55c67729ccdf991136820428d4866c570fabeb
R. Cruz <roberto.cruz@vtsd.com>
2024-05-07 13:54:54 -0400

    Setup CSE 2.1 Repo

--- u331/teacher_instructor_grading.py
+++ u331/teacher_instructor_grading.py
@@ -2,23 +2,45 @@
 print("Please grade your students test submissions below.")
 
 print("Below is the criteria for a written thesis statement. Please grade accordingly.")
-total_grade = 0.0
-essay_score = input("Essay grade: ")
-essay_1 = True
-essay_2 = True
-Thesis_statement = True
+essay_score = 0.0
+Thesis_grade = True
 ev_and_comm = True
 soph_grade = True
 
+
 Thesis_grade = input("Please enter the grade for the written thesis: ")
-if Thesis_statement == "0":
+if Thesis_grade == "0":
     print("Grade: 0")
-    total_grade += 0
-elif Thesis_statement == "1":
+    essay_score += 0
+else:
     print("Grade: 1")
-    total_grade += 1
+    essay_score += 1
 
 ev_and_comm = input("Please enter the grade for evidence & commentary: ")
 if ev_and_comm == "0":
     print("Grade: 0")
-    total_grade += 0
+    essay_score += 0
+elif ev_and_comm == "1":
+    print("Grade: 1")
+    essay_score += 1
+elif ev_and_comm == "2":
+    print("Grade: 2")
+    essay_score += 2
+elif ev_and_comm == "3":
+    print("Grade: 3")
+    essay_score += 3
+else:
+    print("Grade: 4")
+    essay_score += 4
+
+
+soph_grade = input("Please grade student sophistication accordingly: ")
+if soph_grade == "0":
+    print("Grade: 0")
+    essay_score += 0
+else:
+    print("Grade: 1")
+    essay_score += 1
+
+
+print(essay_score)
